1de4162b8f5e61adc99bab323252e42de8aea919
andikscript <andikscript@gmail.com>
2021-08-21 21:37:11 +0700

    first commit

---
+++ src/loginawal/KonfigurasiLogin.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package loginawal;
+
+/**
+ *
+ * @author genz
+ */
+public class KonfigurasiLogin {
+    private String username = "andik";
+    private String password = "andik";
+    public int i = 0;
+    
+    protected String getUser(){
+        return this.username;
+    }
+    
+    protected String getPass(){
+        return this.password;
+    }
+}
---
+++ src/unibacopycenter/MenuUtama.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unibacopycenter;
+import loginawal.*;
+import pegawai.*;
+import pegawai.FormPegawai;
+/**
+ *
+ * @author genz
+ */
+public class MenuUtama {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FormLogin log = new FormLogin();
+        //log.setVisible(true)
+        FormUpdate pegawai = new FormUpdate();
+        pegawai.setVisible(true);
+        ;
+    }
+}
+    
